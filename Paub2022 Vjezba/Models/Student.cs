37a4fcae19233e2002872aef4518c40a01802251
jakov <jakov.knezovic@student.mev.hr>
2022-03-15 17:50:32 +0100

    vjezbe 15.03.2022

---
+++ Paub2022 Vjezba/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paub2022_Vjezba.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string  Ime { get; set; }
+        public string Prezime { get; set; }
+        public char Spol { get; set; }
+        public string Oib { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public int GodinaStudija { get; set;}
+        public bool RedovanStudent { get; set; }
+
+    }
+}
---
+++ Paub2022 Vjezba/Models/StudentiDB.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paub2022_Vjezba.Models
+{
+
+
+    public class StudentiDB
+    {
+        private List<Student> lista = new List<Student>();
+
+
+        public StudentiDB()
+        {
+            lista.Add(new Student()
+            {
+                ID = 1,
+                Prezime = "Ivić",
+                Ime = "Petar",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1991, 02, 01),
+                Oib = "12345678901",
+                RedovanStudent = true
+            });
+            lista.Add(new Student()
+            {
+                ID = 2,
+                Prezime = "Ivanović",
+                Ime = "Marta",
+                Spol = 'Z',
+                DatumRodjenja = new DateTime(1991, 03, 02),
+                Oib = "12345671232",
+                RedovanStudent = true
+            });
+           lista.Add(new Student()
+            {
+                ID = 3,
+                Prezime = "Marić",
+                Ime = "Jakov",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1991, 04, 01),
+                Oib = "11234321231",
+                RedovanStudent = true
+            });
+
+            lista.Add(new Student()
+            {
+                ID = 4,
+                Prezime = "Bedis",
+                Ime = "Barbara",
+                Spol = 'Z',
+                DatumRodjenja = new DateTime(1991, 02, 01),
+                Oib = "12345678901",
+                RedovanStudent = true
+
+            });
+        }
+        public List<Student> VratiListu()
+        {
+            return lista;
+        }
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
